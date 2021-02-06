0d54ef6307a380bd214fd8ec3532af6413c8c4f3
csf <shuangfeng1993@163.com>
2021-02-06 17:59:25 +0800

    添加pwa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,15 @@ module.exports = {
   head: [
     ['link', { rel: 'icon', href: '/assets/img/favicon.ico'}],
     ['meta', { name: 'author', content: '狐七'}],
-    ['meta', {name: 'keywords', content: 'vuepress，狐七的个人博客，html，css，javascript，es6'}]
+    ['meta', {name: 'keywords', content: 'vuepress，狐七的个人博客，html，css，javascript，es6'}],
+    ['link', { rel: 'manifest', href: '/assets/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/assets/icons/icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/assets/icons/icon-72x72.png', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/assets/icons/icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   themeConfig: {
     lastUpdated: '更新时间', // string | boolean
@@ -44,9 +52,24 @@ module.exports = {
     //   ['/page-b','hahaha']
     // ]
   },
-  plugins: {
-    "vuepress-plugin-auto-sidebar": {
+  plugins: [
+    ["vuepress-plugin-auto-sidebar", {
       collapsable: true
-    }
-  }
+    }],
+    ['@vuepress/last-updated', {
+      transformer: (timestamp, lang) => {
+        // 不要忘了安装 moment
+        const moment = require('moment')
+        moment.locale(lang)
+        return moment(timestamp).format('YYYY-MM-DD HH:mm')
+      }
+    }],
+    ['@vuepress/pwa', {
+      serviceWorker: true,
+      updatePopup: {
+      message: "博客更新啦，要刷新看看嘛?",
+      buttonText: "Refresh"
+      }
+    }]
+  ]
 }
