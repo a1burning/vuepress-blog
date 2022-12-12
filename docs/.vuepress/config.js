1f77f763014cef352479d934988f647e5fe0d13f
chengshuangfeng <chengshuangfeng@kuaishou.com>
2022-12-12 20:49:33 +0800

    feat: 添加ICP备案

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,6 +17,11 @@ module.exports = {
     smoothScroll: true,
     subSidebar: 'auto',
     noFoundPageByTencent: false,
+    // 备案
+    record: '京ICP备2021001777号-1',
+    recordLink: 'https://beian.miit.gov.cn/',
+    // cyberSecurityRecord: '公安部备案文案',
+    // cyberSecurityLink: '公安部备案指向链接',
     // sidebar: [
     //   '/',
     //   ['/javascript/es6/','JavaScript'],
